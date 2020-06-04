3a218b88689be4bcb9f6df820bd43da022d3b002
msylla01 <sylla.ahemd@gmail.com>
2020-06-04 20:43:24 +0000

    Formulaire test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { UtlisaateurComponent } from './utlisaateur/utlisaateur.component';
-
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, UtlisaateurComponent ],
